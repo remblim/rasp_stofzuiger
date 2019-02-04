7c794487a8e5d5c0a38a5b882a28eb5d7cc74088
Remblim <44832928+remblim@users.noreply.github.com>
2019-02-04 11:39:14 +0100

    Update scannen.py

--- scannen.py
+++ scannen.py
@@ -5,6 +5,23 @@ from ultrasoonsensor import *
 
 class scanner():
 	def __init__(self):
-		GPIO.setmode(GPIO.BCM(
+		GPIO.setmode(GPIO.BCM)
+		self.ultra_sensor = ultrasonic_sensor()
+		self.stepper = stappenmotor()
+		self.steps_per_round = 520
 		
-
+	def scannen(self):
+		i = 0
+		distance = []
+		while i < self.steps_per_round:
+			distance.extend(self.ultra_sensor.measure_distance())
+			self.stepper.cw(1)
+			i += 1
+		
+		self.stepper.ccw(self.steps_per_round)
+		
+if __name__ == "__main__":
+	scan = scanner()
+	scan.scannen()
+	GPIO.cleanup()
+	print('einde')
