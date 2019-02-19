c793195789718532411ec0ba9169c08f743edc51
Remblim <44832928+remblim@users.noreply.github.com>
2019-02-19 11:19:16 +0100

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -8,3 +8,12 @@ class encoder():
 	
 	def count(self)
 		self.count += 1
+		
+if __name__ == "__main__":
+	import time
+	GPIO.setmode(GPIO.BCM)
+	encoder = encoder(8)
+	while True:
+		time.sleep(0.5)
+		print(encoder.count)		
+	GPIO.cleanup()
