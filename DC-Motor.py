5b661761b952ecee34f4ea0600ec03a8c311e776
remblim <vlekkie_bluppie@hotmail.com>
2019-04-15 19:02:15 +0200

    sadf

--- DC-Motor.py
+++ DC-Motor.py
@@ -1,23 +1,61 @@
 import RPi.GPIO as GPIO
-import time 
+import time
 
 
 class dc_motor():
 	def __init__(self):
-		self.GPIO_output = [12,16,20,21]
+		self.GPIO_output = [8,10,12,16]
 		GPIO.setup(self.GPIO_output,GPIO.OUT)
-	
+
 	def forward_1(self,speed):
-		GPIO.output(self.GPIO_output[0],1)
+		GPIO.output(8,1)
+		GPIO.output(10,0)
 
 	def backward_1(self,speed):
-		GPIO.output(self.GPIO_output[0],0)	
+		GPIO.output(8,0)
+		GPIO.output(10,1)
+
+	def stop_1(self):
+		GPIO.output(8,0)
+		GPIO.output(10,0)
+
+	def forward_2(self,speed):
+		GPIO.output(12,0)
+		GPIO.output(16,1)
+
+	def backward_2(self,speed):
+		GPIO.output(12,1)
+		GPIO.output(16,0)
+
+	def stop_2(self):
+		GPIO.output(12,0)
+		GPIO.output(16,0)
 
 if __name__ == "__main__":
-	GPIO.setmode(GPIO.BCM)
-	motor = dcmotor()
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.cleanup()
+	motor = dc_motor()
+	print('rechts vooruit')
 	motor.forward_1(1)
+	motor.stop_2()
+	time.sleep(10)
+	print('rechts stop')
+	motor.stop_1()
 	time.sleep(1)
+	print('rechts achteruit')
 	motor.backward_1(1)
+	time.sleep(10)
+	motor.stop_1()
+	time.sleep(1)
+	print('links vooruit')
+	motor.forward_2(1)
+	motor.stop_1()
+	time.sleep(10)
+	print('links stop')
+	motor.stop_2()
+	time.sleep(1)
+	print('links achteruit')
+	motor.backward_2(1)
+	time.sleep(10)
 	GPIO.cleanup()
 	print('einde')
