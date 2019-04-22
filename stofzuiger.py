8a71ccc2c0ea28e3472a755fe346193ce6301815
Remblim <44832928+remblim@users.noreply.github.com>
2019-04-22 13:12:10 +0200

    Update stofzuiger.py

--- stofzuiger.py
+++ stofzuiger.py
@@ -12,34 +12,38 @@ Motor = dc_motor()
 server = Server()
 server.setupConnection()
 
-
-
+mode = 0
 
 while run:
-	command = server.dataTransfer()
-	if command == 'a':
-		Motor.forward_1(1)
-		Motor.stop_2()
-		print('left')
-	elif command == 's':
-		Motor.backward_1(1)
-		Motor.backward_2(1)
-		print('backward')
-	elif command == 'd':
-		Motor.stop_1()
-		Motor.forward_2(1)
-		print('right')
-	elif command == 'w':
-		Motor.forward_1(1)
-		Motor.forward_2(1)
-		print('forward')
-	elif command == 'stop':
-		Motor.stop_1()
-		Motor.stop_2()		
-	elif command == 'esc':
-		Motor.stop_1()
-		Motor.stop_2()	
-		break
-	else:
-		Motor.stop_1()
-		Motor.stop_2()	
+	if mode == 0: #drive manual
+		command = server.dataTransfer()
+		if command == 'a':
+			Motor.forward_1(1)
+			Motor.stop_2()
+			print('left')
+		elif command == 's':
+			Motor.backward_1(1)
+			Motor.backward_2(1)
+			print('backward')
+		elif command == 'd':
+			Motor.stop_1()
+			Motor.forward_2(1)
+			print('right')
+		elif command == 'w':
+			Motor.forward_1(1)
+			Motor.forward_2(1)
+			print('forward')
+		elif command == 'stop':
+			Motor.stop_1()
+			Motor.stop_2()		
+		elif command == 'esc':
+			Motor.stop_1()
+			Motor.stop_2()	
+			break
+		
+		else:
+			Motor.stop_1()
+			Motor.stop_2()
+	
+	elif mode == 1:
+		
