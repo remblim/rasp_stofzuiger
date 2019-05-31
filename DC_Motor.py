727dc6892df8b98152fe323c1af0bec931fde125
Remblim <44832928+remblim@users.noreply.github.com>
2019-05-31 12:39:30 +0200

    Update DC_Motor.py

--- DC_Motor.py
+++ DC_Motor.py
@@ -4,33 +4,33 @@ import time
 
 class dc_motor():
 	def __init__(self):
-		self.GPIO_output = [14,15,18,23]
+		self.GPIO_output = [8,10,12,16]
 		GPIO.setup(self.GPIO_output,GPIO.OUT)
 
 	def forward_right(self,speed):
-		GPIO.output(14,1)
-		GPIO.output(15,0)
+		GPIO.output(8,1)
+		GPIO.output(10,0)
 
 	def backward_right(self,speed):
-		GPIO.output(14,0)
-		GPIO.output(15,1)
+		GPIO.output(8,0)
+		GPIO.output(10,1)
 		print('1 backward')
 	def stop_right(self):
-		GPIO.output(14,0)
-		GPIO.output(15,0)
+		GPIO.output(8,0)
+		GPIO.output(10,0)
 
 	def forward_left(self,speed):
-		GPIO.output(18,0)
-		GPIO.output(23,1)
+		GPIO.output(12,0)
+		GPIO.output(16,1)
 
 	def backward_left(self,speed):
-		GPIO.output(18,1)
-		GPIO.output(23,0)
+		GPIO.output(12,1)
+		GPIO.output(16,0)
 		print('2 backward')
 
 	def stop_left(self):
-		GPIO.output(18,0)
-		GPIO.output(23,0)
+		GPIO.output(12,0)
+		GPIO.output(16,0)
 
 if __name__ == "__main__":
 	GPIO.setmode(GPIO.BCM)
