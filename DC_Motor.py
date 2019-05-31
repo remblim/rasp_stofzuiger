d52d01982e6706d14a8889d0400c82b0d6429ccb
Remblim <44832928+remblim@users.noreply.github.com>
2019-05-31 20:33:48 +0200

    Update DC_Motor.py

--- DC_Motor.py
+++ DC_Motor.py
@@ -6,35 +6,34 @@ class dc_motor():
 	def __init__(self):
 		self.GPIO_output = [8,10,12,16]
 		GPIO.setup(self.GPIO_output,GPIO.OUT)
-		one = GPIO.PWM(8,1000).start(0)
-		two = GPIO.PWM(10,1000).start(0)
-		three = GPIO.PWM(12,1000).start(0)
-		forr = GPIO.PWM(16,1000).start(0)
+		self.one = GPIO.PWM(8,1000).start(0)
+		self.two = GPIO.PWM(10,1000).start(0)
+		self.three = GPIO.PWM(12,1000).start(0)
+		self.forr = GPIO.PWM(16,1000).start(0)
 		
 	def forward_right(self,speed):
-		one.ChangeDutyCycle(speed*100)
-		two.ChangeDutyCycle(0)
+		self.one.ChangeDutyCycle(speed*100)
+		self.two.ChangeDutyCycle(0)
 
 	def backward_right(self,speed):
-		one.ChangeDutyCycle(0)
-		two.ChangeDutyCycle(speed*100)
-		print('1 backward')
+		self.one.ChangeDutyCycle(0)
+		self.two.ChangeDutyCycle(speed*100)
 		
 	def stop_right(self):
-		one.ChangeDutyCycle(0)
-		two.ChangeDutyCycle(0)
+		self.one.ChangeDutyCycle(0)
+		self.two.ChangeDutyCycle(0)
 
 	def forward_left(self,speed):
-		three.ChangeDutyCycle(0)
-		forr.ChangeDutyCycle(speed*100)
+		self.three.ChangeDutyCycle(0)
+		self.forr.ChangeDutyCycle(speed*100)
 
 	def backward_left(self,speed):
-		three.ChangeDutyCycle(0)
-		forr.ChangeDutyCycle(speed*100)
+		self.three.ChangeDutyCycle(0)
+		self.forr.ChangeDutyCycle(speed*100)
 
 	def stop_left(self):
-		three.ChangeDutyCycle(0)
-		forr.ChangeDutyCycle(0)
+		self.three.ChangeDutyCycle(0)
+		self.forr.ChangeDutyCycle(0)
 
 if __name__ == "__main__":
 	GPIO.setmode(GPIO.BOARD)
