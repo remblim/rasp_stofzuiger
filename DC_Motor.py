8707141d4b2681d3070b0272b5252143d45c9b98
Remblim <44832928+remblim@users.noreply.github.com>
2019-05-31 11:17:31 +0200

    Update DC_Motor.py

--- DC_Motor.py
+++ DC_Motor.py
@@ -7,28 +7,28 @@ class dc_motor():
 		self.GPIO_output = [8,22,12,16]
 		GPIO.setup(self.GPIO_output,GPIO.OUT)
 
-	def forward_1(self,speed):
+	def forward_right(self,speed):
 		GPIO.output(8,1)
 		GPIO.output(22,0)
 
-	def backward_1(self,speed):
+	def backward_right(self,speed):
 		GPIO.output(8,0)
 		GPIO.output(22,1)
 		print('1 backward')
-	def stop_1(self):
+	def stop_right(self):
 		GPIO.output(8,0)
 		GPIO.output(22,0)
 
-	def forward_2(self,speed):
+	def forward_left(self,speed):
 		GPIO.output(12,0)
 		GPIO.output(16,1)
 
-	def backward_2(self,speed):
+	def backward_left(self,speed):
 		GPIO.output(12,1)
 		GPIO.output(16,0)
 		print('2 backward')
 
-	def stop_2(self):
+	def stop_left(self):
 		GPIO.output(12,0)
 		GPIO.output(16,0)
 
@@ -37,26 +37,27 @@ if __name__ == "__main__":
 	GPIO.cleanup()
 	motor = dc_motor()
 	print('rechts vooruit')
-	motor.forward_1(1)
+	motor.forward_right(1)
 	motor.stop_1()
 	time.sleep(10)
 	print('rechts stop')
-	motor.stop_1()
+	motor.stop_right()
 	time.sleep(1)
 	print('rechts achteruit')
-	motor.backward_1(1)
+	motor.backward_right(1)
 	time.sleep(10)
-	motor.stop_1()
+	print('rechts stop')
+	motor.stop_right()
 	time.sleep(1)
 	print('links vooruit')
-	motor.forward_2(1)
-	motor.stop_1()
+	motor.forward_left(1)
+	motor.stop_left()
 	time.sleep(10)
 	print('links stop')
-	motor.stop_2()
+	motor.stop_left()
 	time.sleep(1)
 	print('links achteruit')
-	motor.backward_2(1)
+	motor.backward_left(1)
 	time.sleep(10)
 	GPIO.cleanup()
 	print('einde')
