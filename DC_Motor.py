3913ad5a020f087ef69cc4c886ecdd23d7f1a672
Remblim <44832928+remblim@users.noreply.github.com>
2019-06-04 20:07:15 +0200

    Update DC_Motor.py

--- DC_Motor.py
+++ DC_Motor.py
@@ -57,6 +57,13 @@ class dc_motor():
 				self.l_speed = self.max_speed-self.min_speed
 			if self.l_speed < -self.max_speed-self.min_speed:
 				self.l_speed = -self.max_speed-self.min_speed
+		
+		if self.r_speed < 0.1 and self.r_target_speed == 0:
+			self.r_speed = 0
+			
+		if self.l_speed < 0.1 and self.l_target_speed == 0:
+			self.l_speed = 0
+
 		print(self.r_speed,self.l_speed)
 		if self.r_speed < 0:
 			self.speed_one = 0
