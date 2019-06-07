98734c4860f7b9c8814e058e414a1faac257de6b
Remblim <44832928+remblim@users.noreply.github.com>
2019-06-07 14:59:40 +0200

    Update DC_Motor.py

--- DC_Motor.py
+++ DC_Motor.py
@@ -85,6 +85,7 @@ class dc_motor():
 			self.speed_three = 0
 			self.speed_forr = 0
 		
+		print(self.speed_forr,self.speed_three)
 		self.one.ChangeDutyCycle(self.speed_one*100)
 		self.two.ChangeDutyCycle(self.speed_two*100)
 		self.three.ChangeDutyCycle(self.speed_three*100)
