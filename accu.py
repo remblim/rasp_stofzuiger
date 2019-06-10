6b084f49ac6215e630b55fc220d43769fb8f56f0
Remblim <44832928+remblim@users.noreply.github.com>
2019-06-10 13:05:39 +0200

    Update accu.py

--- accu.py
+++ accu.py
@@ -5,7 +5,7 @@ class battery():
 	def __init__(self):
 		self.adc = Adafruit_ADS1x15.ADS1115()
 		self.gain = 2/3
-		self.battery = [0]*4
+		self.battery = 0
 	
 	def step(self):
 		self.battery = (self.adc.read_adc(3, gain=self.gain)/32767)*6.144
